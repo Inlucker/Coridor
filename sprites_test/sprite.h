76c11fe7709cae49cff4eeba6c3cf810e19d145f
Dmitry Luschan <dluschan@gmail.com>
2017-10-25 01:02:30 +0300

    merged

--- sprites_test/sprite.h
+++ sprites_test/sprite.h
@@ -1,34 +1,33 @@
 #ifndef SPRITE_H
 #define SPRITE_H
 
-#include <QObject>
 #include <QGraphicsItem>
-#include <QTimer>
-#include <QPixmap>
+#include <QObject>
 #include <QPainter>
+#include <QPixmap>
+#include <QTimer>
 
 class Sprite : public QObject, public QGraphicsItem
 {
-    Q_OBJECT
+	Q_OBJECT
 public:
-    explicit Sprite(QObject *parent = 0);
+	explicit Sprite(QObject* parent = 0);
 
 signals:
 
 public slots:
 
 private slots:
-    void nextFrame();   // Слот для пролистывания изображения в QPixmap
+	void nextFrame(); // Слот для пролистывания изображения в QPixmap
 
 private:
-    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
-    QRectF boundingRect() const;
+	void paint(QPainter* painter, const QStyleOptionGraphicsItem* option, QWidget* widget);
+	QRectF boundingRect() const;
 
 private:
-    QTimer *timer;      // Таймер для пролистывания изображения в QPixmap
-    QPixmap *spriteImage;   // В данный объект QPixamp будет помещён спрайт
-    int currentFrame;   // Координата X, с которой начинает очередной кадр спрайта
-
+	QTimer* timer;		  // Таймер для пролистывания изображения в QPixmap
+	QPixmap* spriteImage; // В данный объект QPixamp будет помещён спрайт
+	int currentFrame; // Координата X, с которой начинает очередной кадр спрайта
 };
 
 #endif // SPRITE_H
